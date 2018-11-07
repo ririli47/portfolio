2e025da21051266b023f2806016f60f7af74043c
ririli47 <suguha47@gmail.com>
2018-11-07 13:26:28 +0900

    djustment works specificaton

--- components/MyWorks.vue
+++ components/MyWorks.vue
@@ -1,16 +1,26 @@
 <template>
     <section class="works-list">
         <div v-if="show" class="infomation">
+            <div v-on:click="showIdChange(-1)">
+                hoge
+            </div>
             <div class="card">
                 <div class="card-image infromation-card">
-                    <img :src="works[showId-1].logo">
+                    <img :src="works[showId].logo">
                 </div>
                 <div class="card-content">
-                    せつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよせつめいだよ
+                    <dl>
+                        <dt>Name</dt><dd>{{ works[showId].name }}</dd>
+                        <dt>Techs</dt><dd>{{ works[showId].techs }}</dd>
+                        <dt>Infomaiton</dt><dd>{{ works[showId].infomaiton }}</dd>
+                    </dl>
                 </div>
                 <div class="card-action">
-                    <a href="#">リンクだよ</a>
+                    <a :href="works[showId].link" target="_blank">サイトへのリンク</a>
                 </div>
+            </div>
+            <div v-on:click="showIdChange(+1)">
+                fuga
             </div>
         </div>
 
@@ -21,7 +31,7 @@
                 </div>
                 <div class="card-content">
                     <span class="card-title activator grey-text text-darken-4">{{ work.name }}</span>
-                    <p><a :href="work.link" target="_blink" >{{ work.name }}</a></p>
+                    <a :href="work.link" target="_blink" ><p>{{ work.name }}</p></a>
                 </div>
             </div>
         </div>
@@ -34,14 +44,12 @@
         data() {
             return {
                 works: [
-                    {id: 1, name: 'Tasker', link: 'https://ririli-tasker.herokuapp.com/', logo: require("~/assets/img/tasker_logo.png")},
-                    {id: 2, name: 'IdeaCafe', link: 'https://ideacafe.herokuapp.com/', logo: require("~/assets/img/ideacafe_logo.png")},
-                    {id: 3, name: 'Portfolio', link: 'https://www.ririli.net/', logo: require("~/assets/img/ririli.jpg")},
-                ],
-                infomations: [
-                    {id: 1, name: 'Tasker', link: 'https://ririli-tasker.herokuapp.com/', logo: require("~/assets/img/tasker_logo.png")},
-                    {id: 2, name: 'IdeaCafe', link: 'https://ideacafe.herokuapp.com/', logo: require("~/assets/img/ideacafe_logo.png")},
-                    {id: 3, name: 'Portfolio', link: 'https://www.ririli.net/', logo: require("~/assets/img/ririli.jpg")},
+                    {id: 0, name: 'Tasker', link: 'https://ririli-tasker.herokuapp.com/', logo: require("~/assets/img/tasker_logo.png"),
+                    techs: 'Ruby on Rails, heroku', infomaiton: 'RoRのチュートリアルを一周したのちに練習で作ったTodo管理ツールです。初めてwebフレームワークを利用した成果物です。'},
+                    {id: 1, name: 'IdeaCafe', link: 'https://ideacafe.herokuapp.com/', logo: require("~/assets/img/ideacafe_logo.png"),
+                    techs: 'Laravel, Docker, Github, heroku, PostgreSQL', infomaiton: 'PHPフレームワークであるLaravelの練習に作りったアイデアを共有するというコンセプトの簡易SNSです。'},
+                    {id: 2, name: 'Portfolio', link: 'https://www.ririli.net/', logo: require("~/assets/img/ririli.jpg"),
+                    techs: 'Vue.js, Nuxt.js, Github, Netlify', infomaiton: '今ご覧いただいているPortfolioです。Vue.jsとNetlify使ってみたさで作りました。コンポーネント指向いいです。'},
                 ],
                 show: false,
                 showId: 0
@@ -56,7 +64,17 @@
                     this.show = !this.show
                 }
                 this.showId = id
+            },
+            showIdChange(change) {
+                this.showId = this.showId + change
+                if(this.showId < 0) {
+                    this.showId = this.works.length -1
+                }
+                else if (this.showId > this.works.length -1) {
+                    this.showId = 0
+                }
             }
+
         }
     }
 </script>
@@ -88,8 +106,28 @@
     }
     .infromation-card {
         width: 20%;
-        padding-top: 5%;
+        padding-top: 3%;
     }
+
+    dl {
+        font-size: 1.3rem;
+        width: 100%;
+        line-height: 2.5rem;
+    }
+    dt {
+        width: 20%;
+        margin-left: 5%;
+        text-align: left;
+        border-bottom: 1px solid red;
+    }
+    dd {
+        margin-left: 15%;
+        margin-bottom: 3%;
+        margin-right: 5%;
+        text-align: left;
+        border-bottom: 1px solid red;
+    }
+
 
     @media (max-width: 767px) {
         .topic {
